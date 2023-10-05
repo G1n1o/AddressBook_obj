6e885b272557a4cad13e372a4d71719a1ec4c11c
Rafal Szkudlarek <rafal.szkudlarek.programista@gmail.com>
2023-10-05 05:17:46 +0200

    Added base class AddressBook and User

---
+++ AddressBook.h
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include "User.h"
+
+using namespace std;
+
+class AddresBook {
+
+    int idUser;
+    int idLastUser;
+    int idDeletedUser;
+
+    vector <User> users;
+
+    public:
+    void userRejestration();
+
+
+};
--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include "AddressBook.h"
 
 using namespace std;
 
 int main()
 {
+    AddresBook adressBook;
 
     return 0;
 }
